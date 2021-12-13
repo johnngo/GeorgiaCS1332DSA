42a3161bda04f69ae4d7531ea2a7898f1707d47d
John Ngo <ngongo_jn@hotmail.com>
2021-12-12 17:19:14 -0800

    update file

--- BST.java
+++ BST.java
@@ -1,5 +1,8 @@
 
 import java.util.NoSuchElementException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 /**
  * Your implementation of a BST.
@@ -23,9 +26,9 @@ public class BST<T extends Comparable<? super T>> {
      *
      * The new data should become a leaf in the tree.
      *
-     * Traverse the tree to find the appropriate location. If the data is
-     * already in the tree, then nothing should be done (the duplicate
-     * shouldn't get added, and size should not be incremented).
+     * Traverse the tree to find the appropriate location. If the data is already in
+     * the tree, then nothing should be done (the duplicate shouldn't get added, and
+     * size should not be incremented).
      *
      * Should be O(log n) for best and average cases and O(n) for worst case.
      *
@@ -34,21 +37,23 @@ public class BST<T extends Comparable<? super T>> {
      */
     public void add(T data) {
         // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
-        if ( data == null) {
-            throw new IllegalArgumentException("Cannot add null to BST");
+        if (data == null) {
+            throw new IllegalArgumentException("Cannot add null to BST.");
         }
+
         root = _add(root, data);
     }
 
     private BSTNode<T> _add(BSTNode<T> node, T data) {
-        if (node== null){
+        if (node == null) {
             size++;
             return new BSTNode<T>(data);
         } else if (data.compareTo(node.getData()) < 0) {
             node.setLeft(_add(node.getLeft(), data));
         } else if (data.compareTo(node.getData()) > 0) {
-            node.setRight(_add(node.getRigth(), data));
+            node.setRight(_add(node.getRight(), data));
         }
+
         return node;
     }
 
@@ -57,18 +62,15 @@ public class BST<T extends Comparable<? super T>> {
      *
      * This must be done recursively.
      *
-     * There are 3 cases to consider:
-     * 1: The node containing the data is a leaf (no children). In this case,
-     * simply remove it.
-     * 2: The node containing the data has one child. In this case, simply
-     * replace it with its child.
-     * 3: The node containing the data has 2 children. Use the SUCCESSOR to
-     * replace the data. You should use recursion to find and remove the
-     * successor (you will likely need an additional helper method to
-     * handle this case efficiently).
+     * There are 3 cases to consider: 1: The node containing the data is a leaf (no
+     * children). In this case, simply remove it. 2: The node containing the data
+     * has one child. In this case, simply replace it with its child. 3: The node
+     * containing the data has 2 children. Use the SUCCESSOR to replace the data.
+     * You should use recursion to find and remove the successor (you will likely
+     * need an additional helper method to handle this case efficiently).
      *
-     * Do NOT return the same data that was passed in. Return the data that
-     * was stored in the tree.
+     * Do NOT return the same data that was passed in. Return the data that was
+     * stored in the tree.
      *
      * Hint: Should you use value equality or reference equality?
      *
@@ -81,13 +83,60 @@ public class BST<T extends Comparable<? super T>> {
      */
     public T remove(T data) {
         // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        if (data == null) {
+            throw new IllegalArgumentException("Cannot remove null from BST.");
+        }
+
+        ArrayList<T> dataAndSuccessor = new ArrayList<>(2);
+        dataAndSuccessor.add(null);
+        dataAndSuccessor.add(null);
+        root = _remove(root, data, dataAndSuccessor);
+
+        return dataAndSuccessor.get(0);
+    }
+    
+    private BSTNode<T> _remove(BSTNode<T> node, T data, ArrayList<T> dataAndSuccessor) {
+        if (node == null) {
+            throw new NoSuchElementException("Cannot remove from BST: data not found.");
+        } else if (data.compareTo(node.getData()) < 0) {
+            node.setLeft(_remove(node.getLeft(), data, dataAndSuccessor));
+        } else if (data.compareTo(node.getData()) > 0) {
+            node.setRight(_remove(node.getRight(), data, dataAndSuccessor));
+        } else {
+            dataAndSuccessor.set(0, node.getData());
+            size--;
+    
+            if (node.getLeft() == null && node.getRight() == null) {
+                return null;
+            } else if (node.getRight() == null) {
+                return node.getLeft();
+            } else if (node.getLeft() == null) {
+                return node.getRight();
+            } else {
+                node.setRight(_removeSuccessor(node.getRight(), dataAndSuccessor));
+                node.setData(dataAndSuccessor.get(1));
+            }
+        }
+
+        return node;
+    }
+
+    private BSTNode<T> _removeSuccessor(BSTNode<T> node, ArrayList<T> dataAndSuccessor) {
+        if (node.getLeft() == null) {
+            dataAndSuccessor.set(1, node.getData());
+            return node.getRight();
+        } else {
+            node.setLeft(_removeSuccessor(node.getLeft(), dataAndSuccessor));
+        }
+
+        return node;
     }
 
     /**
      * Returns the root of the tree.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
+     * For grading purposes only. You shouldn't need to use this method since you
+     * have direct access to the variable.
      *
      * @return The root of the tree
      */
@@ -99,8 +148,8 @@ public class BST<T extends Comparable<? super T>> {
     /**
      * Returns the size of the tree.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
+     * For grading purposes only. You shouldn't need to use this method since you
+     * have direct access to the variable.
      *
      * @return The size of the tree
      */
@@ -108,4 +157,5 @@ public class BST<T extends Comparable<? super T>> {
         // DO NOT MODIFY THIS METHOD!
         return size;
     }
+
 }
